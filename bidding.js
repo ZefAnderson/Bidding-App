5c392bc9b035b63e0ff4e653957ad866b8bf7d97
Zef Anderson <zeferino@Zefs-MacBook-Pro.local>
2023-03-15 21:55:34 -0600

    getting close to working

--- bidding.js
+++ bidding.js
@@ -1,28 +1,56 @@
+document.getElementById("bidDisplay").onload = showHistory();
+
+function showHistory() {
+    let history = JSON.parse(localStorage.getItem("userBids"));
+    document.getElementById("bidDisplay").innerHTML = history;
+}
+
 const bids = [
     {
         user: "User1",
         bid: "$0.00"
+    },
+    {
+        user: "User2",
+        bid: "0.00"
     }
 ]
-const currentBid = bids[0];
-
-function currentBids(){
-    document.getElementById("bidDisplay").innerHTML = "Let the Bidding Begin";
-}
-window.onload = currentBids();
-
 
 function render() {
-    document.getElementById("bidDisplay").innerHTML = bids;
+    let temp = JSON.parse(localStorage.getItem("userBids"));
+    let bidsHtml = "<ul class='list-group'>";
+    bidsHtml += "</ul>"
+    temp.forEach((list) => {
+        bidsHtml += `<li class="list-group-item">${list.user} ${list.bid}</li>`;
+      });
+    document.getElementById("bidDisplay").innerHTML = bidsHtml;
 }
-function placebid1() {
+
+function placeBid1() {
     let text = document.getElementById("user1Bid").value;
     if(text){
-        currentBid.push({
+        bids.push({
             user: "User1",
             bid: "$" + text + ".00"
         })
+        let serialized = JSON.stringify(bids);
+        localStorage.setItem("userBids", serialized);
         render();
     }
     document.getElementById("user1Bid").value = '';
 }
+function placeBid2() {
+    let text = document.getElementById("user2Bid").value;
+    if(text){
+        bids.push({
+            user: "User2",
+            bid: "$" + text + ".00"
+        })
+        let serialized = JSON.stringify(bids);
+        localStorage.setItem("userBids", serialized);
+        render();
+    }
+    document.getElementById("user2Bid").value = '';
+}
+
+
